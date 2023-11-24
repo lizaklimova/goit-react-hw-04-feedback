ea9154b4c973814dc5801ee3415163d7dc966a82
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2023-11-24 12:38:23 +0200

    fixed corrections

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,15 +19,15 @@ export default function App() {
 
     switch (name) {
       case 'good':
-        setGood(good + 1);
+        setGood(good => good + 1);
         break;
 
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(neutral => neutral + 1);
         break;
 
       case 'bad':
-        setBad(bad + 1);
+        setBad(bad => bad + 1);
         break;
 
       default:
--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+
 export const Container = styled.section`
   display: flex;
   flex-direction: column;
@@ -7,7 +8,7 @@ export const Container = styled.section`
   gap: 25px;
   min-height: 285px;
   padding: 30px;
-  border-bottom: 1px solid black;
+  border-bottom: 1.3px solid black;
   text-align: center;
   background: linear-gradient(270deg, #d1ddab, #d4c5a7, #e4c3d0);
   box-shadow: inset 0px 0px 180px 5px #ffffff;
